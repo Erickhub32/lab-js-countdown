14c82e3f3782a0e56f2262f89f9cf9c9c77f128b
Erickhub32 <erick.arevalolopez25@gmail.com>
2024-07-11 21:01:55 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
+//< !--Time Remaining-- >
+//<div id="time">10</div>
+
 
 
 // ITERATION 1: Add event listener to the start button
@@ -9,15 +12,49 @@ let timer = null; // Variable to store the interval
 // Your code goes here ...
 
 
+document.querySelector("#start-btn").onclick = () => {
+  startCountdown()
+
+}
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
+
   console.log("startCountdown called!");
 
+  timer = setInterval(() => {
+    remainingTime--
 
-  // Your code goes here ...
+
+
+
+    if (remainingTime === 0) {
+      clearInterval(setIntervalId)
+
+    }
+
+  }, 1000)
+
+
 }
+let counter = 0
+
+const intervalId = setInterval(() => {
+  counter--
+  document.querySelector('h1 span').innerHTML = counter
+
+  if (remainingTime === 0) clearInterval()
+}, 1000)
+
+document.querySelector('button').onclick = () => {
+  console.log('El intervalo con ID número', intervalId, 'ha sido detenido')
+  clearInterval(intervalId)
+}
+
+
+
+
 
 
 
@@ -36,3 +73,14 @@ function showToast(message) {
   // Your code goes here ...
 
 }
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
